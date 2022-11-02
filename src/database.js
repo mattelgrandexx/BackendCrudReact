dea81c5bc9206dfbd92dacbbda23d30c8ff6c163
matias cardoso <mattiascardoso10@gmail.com>
2022-11-01 22:50:57 -0300

    se agrego link de produccion

--- src/database.js
+++ src/database.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
-const url = "mongodb://localhost:27017/cafe-benito-santos";
+// const url = "mongodb://localhost:27017/cafe-benito-santos"; //local
+const url = "mongodb+srv://mattelgrandexx:matt12345@cluster0.s1wboe0.mongodb.net/cafe-benito-santos"; //produccion
 
 mongoose.connect(url);
 
 const connection = mongoose.connection;
 
 connection.once("open", ()=>{
-    console.log("alakazam base de datos conectada")
+    console.log("base de datos conectada")
 })
